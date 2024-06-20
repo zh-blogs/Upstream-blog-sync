e45b0fe395c7e0052517c6eae1768376aa49b7e2
Mayx <i@mayx.eu.org>
2024-06-20 23:36:52 +0800

    Update load_from_findblogs.py

--- load_from_findblogs.py
+++ load_from_findblogs.py
@@ -5,6 +5,7 @@ import uuid
 
 sites = []
 for i in range(1,10000000):
+  print(i)
   d = requests.get("https://bf.zzxworld.com/api/sites/" + str(i)).json()
   sites += d["sites"]
   if not d["has_more_page"]:
@@ -18,5 +19,7 @@ for i in sites:
   if urllib.parse.urlparse(i["url"]).netloc in r:
     newlist.append(i)
 
+print(newlist)
+
 for i in newlist:
-  requests.put("https://zhblogs.ohyee.cc/api/blog", data=json.dumps({"blog":{"id":str(uuid.uuid4()), "name": i["title"], "url": i["url"], "status": "unknown", "tags": [], "repeat": false, "enabled": false, "saveweb_id": "", "feed": i["rss_url"], "recommend": false}})).text
+  requests.put("https://zhblogs.ohyee.cc/api/blog", data=json.dumps({"blog":{"id":str(uuid.uuid4()), "name": i["title"], "url": i["url"], "status": "unknown", "tags": [], "repeat": False, "enabled": False, "saveweb_id": "", "feed": i["rss_url"], "recommend": False}})).text
